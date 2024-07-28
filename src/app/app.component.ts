fbaac24e234b3392481658790bbf7cad691ab31a
mohammedhosni25@gmail.com <mohammedhosni25@gmail.com>
2024-07-29 00:56:07 +0300

    Day4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductsComponent } from './components/products/products.component';
+import { HomeComponent } from './components/home/home.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { CartPageComponent } from './components/cart-page/cart-page.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ProductsComponent,HomeComponent,ContactUsComponent,PageNotFoundComponent,CartPageComponent,AboutUsComponent],
+
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/products/products.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../../services/products.service';
+import { IProduct } from '../../models/IProduct';
+import { CurrencyPipe } from '@angular/common';
+
+@Component({
+  selector: 'app-products',
+  standalone: true,
+  imports: [CurrencyPipe],
+  templateUrl: './products.component.html',
+  styleUrl: './products.component.css'
+})
+export class ProductsComponent implements OnInit{
+
+  allProducts:IProduct[]=[]
+  
+  constructor(public productServ:ProductsService ){
+
+  }
+
+    ngOnInit(): void {
+      this.allProducts = this.productServ.getAllProducts()
+    }
+}
